86573539b8a30d45c8eb0f4dd03304eec917e278
insilica <mwhitmire2@student.gsu.edu>
2017-04-02 00:24:57 -0400

    When you forget to commit the things you intended to commit the last time..

--- src/edu/gsu/csc1302/texasholdem/GameEngine.java
+++ src/edu/gsu/csc1302/texasholdem/GameEngine.java
@@ -1,10 +1,56 @@
 package edu.gsu.csc1302.texasholdem;
 
-import edu.gsu.csc1302.poker.mwhitmire.*;
-import edu.gsu.csc1302.coll1.mwhitmire.*;
+import java.util.Scanner;
 
-public class TexasHoldem {
+public class GameEngine {
+	private Table gameTable;
 
-	//TODO: Create the game engine.
+	public void startGame() {
+		setGameTable(new Table());
+		createUser();
+		
+	}
+	
+	public void gameRound() {
+		dealHand();
+	}
+
+	public void createUser(){
+		String name;
+		int bankroll;
+		
+		Scanner userScanner = new Scanner(System.in);
+		System.out.println("Enter a username: ");
+		name = userScanner.nextLine();
+		System.out.println("Enter a bankroll amount: ");
+		bankroll = userScanner.nextInt();
+		
+		Player user = new Player(bankroll, name);
+		gameTable.addPlayerToTable(user);
+		userScanner.close();
+		
+	}
+	
+	public void dealHand(){
+		for(int i = 0; i < 2; i++){	
+			for(Player player : gameTable.getGamePlayers()){
+				player.dealToPlayer(gameTable.getGameDeck().dealFromTop());
+			}
+		}
+	}
+	
+	
+	
+	
+	//getters and setters
+	public Table getGameTable() {
+		return gameTable;
+	}
+
+	public void setGameTable(Table gameTable) {
+		this.gameTable = gameTable;
+	}
+	
+	
 	
 }
--- src/edu/gsu/csc1302/texasholdem/test/tester.java
+++ src/edu/gsu/csc1302/texasholdem/test/tester.java
@@ -2,9 +2,6 @@ package edu.gsu.csc1302.texasholdem.test;
 
 
 
-import java.awt.List;
-import java.util.ArrayList;
-
 import edu.gsu.csc1302.texasholdem.*;
 
 //The testbed for all classes. Feel free to manipulate this as you see
@@ -18,50 +15,50 @@ public class tester {
 		
 		
 		
-		Table gameTable = new Table();
-		Player player1 = new Player(1000, "Deckard");
-		Player player2 = new Player(1000, "Roy");
-		Player player3 = new Player(1000, "Pris");
-		Player player4 = new Player(1000, "Leon");
-		
-		gameTable.addPlayerToTable(player1);
-		gameTable.addPlayerToTable(player2);
-		gameTable.addPlayerToTable(player3);
-		gameTable.addPlayerToTable(player4);
-
-		
-		
-		
-		gameTable.getGamePlayers().get(0).dealToPlayer(gameTable.getGameDeck().dealFromTop());
-		gameTable.getGamePlayers().get(0).dealToPlayer(gameTable.getGameDeck().dealFromTop());
-		gameTable.getGamePlayers().get(1).dealToPlayer(gameTable.getGameDeck().dealFromTop());
-		gameTable.getGamePlayers().get(1).dealToPlayer(gameTable.getGameDeck().dealFromTop());
-		gameTable.getGamePlayers().get(2).dealToPlayer(gameTable.getGameDeck().dealFromTop());
-		gameTable.getGamePlayers().get(2).dealToPlayer(gameTable.getGameDeck().dealFromTop());
-		gameTable.getGamePlayers().get(3).dealToPlayer(gameTable.getGameDeck().dealFromTop());
-		gameTable.getGamePlayers().get(3).dealToPlayer(gameTable.getGameDeck().dealFromTop());
-		gameTable.getCommunityCards().addAll(gameTable.getGameDeck().dealFlop());
-		gameTable.getCommunityCards().add(gameTable.getGameDeck().dealFromTop());
-		gameTable.getCommunityCards().add(gameTable.getGameDeck().dealFromTop());
-		
-		gameTable.roundWinner();
-	
-		
-	
-		
-		
-		
-		System.out.println("Player1 Cards: " + gameTable.getGamePlayers().get(0).getCards());
-		System.out.println("Player2 Cards: " + gameTable.getGamePlayers().get(1).getCards());
-		System.out.println("Player3 Cards: " + gameTable.getGamePlayers().get(2).getCards());
-		System.out.println("Player4 Cards: " + gameTable.getGamePlayers().get(3).getCards());
-		System.out.println("Community Cards: " + gameTable.getCommunityCards());
-		System.out.println("Player " + gameTable.getGamePlayers().get(0).getName() + " has " + gameTable.getGamePlayers().get(0).getHandRank());
-		System.out.println("Player " + gameTable.getGamePlayers().get(1).getName() + " has " + gameTable.getGamePlayers().get(1).getHandRank());
-		System.out.println("Player " + gameTable.getGamePlayers().get(2).getName() + " has " + gameTable.getGamePlayers().get(2).getHandRank());
-		System.out.println("Player " + gameTable.getGamePlayers().get(3).getName() + " has " + gameTable.getGamePlayers().get(3).getHandRank());
-		System.out.println("Winner " + gameTable.getWinner().getName() );
-		
+//		Table gameTable = new Table();
+//		Player player1 = new Player(1000, "Deckard");
+//		Player player2 = new Player(1000, "Roy");
+//		Player player3 = new Player(1000, "Pris");
+//		Player player4 = new Player(1000, "Leon");
+//		
+//		gameTable.addPlayerToTable(player1);
+//		gameTable.addPlayerToTable(player2);
+//		gameTable.addPlayerToTable(player3);
+//		gameTable.addPlayerToTable(player4);
+//
+//		
+//		
+//		
+//		gameTable.getGamePlayers().get(0).dealToPlayer(gameTable.getGameDeck().dealFromTop());
+//		gameTable.getGamePlayers().get(0).dealToPlayer(gameTable.getGameDeck().dealFromTop());
+//		gameTable.getGamePlayers().get(1).dealToPlayer(gameTable.getGameDeck().dealFromTop());
+//		gameTable.getGamePlayers().get(1).dealToPlayer(gameTable.getGameDeck().dealFromTop());
+//		gameTable.getGamePlayers().get(2).dealToPlayer(gameTable.getGameDeck().dealFromTop());
+//		gameTable.getGamePlayers().get(2).dealToPlayer(gameTable.getGameDeck().dealFromTop());
+//		gameTable.getGamePlayers().get(3).dealToPlayer(gameTable.getGameDeck().dealFromTop());
+//		gameTable.getGamePlayers().get(3).dealToPlayer(gameTable.getGameDeck().dealFromTop());
+//		gameTable.getCommunityCards().addAll(gameTable.getGameDeck().dealFlop());
+//		gameTable.getCommunityCards().add(gameTable.getGameDeck().dealFromTop());
+//		gameTable.getCommunityCards().add(gameTable.getGameDeck().dealFromTop());
+//		
+//		gameTable.roundWinner();
+//	
+//		
+//	
+//		
+//		
+//		
+//		System.out.println("Player1 Cards: " + gameTable.getGamePlayers().get(0).getCards());
+//		System.out.println("Player2 Cards: " + gameTable.getGamePlayers().get(1).getCards());
+//		System.out.println("Player3 Cards: " + gameTable.getGamePlayers().get(2).getCards());
+//		System.out.println("Player4 Cards: " + gameTable.getGamePlayers().get(3).getCards());
+//		System.out.println("Community Cards: " + gameTable.getCommunityCards());
+//		System.out.println("Player " + gameTable.getGamePlayers().get(0).getName() + " has " + gameTable.getGamePlayers().get(0).getHandRank());
+//		System.out.println("Player " + gameTable.getGamePlayers().get(1).getName() + " has " + gameTable.getGamePlayers().get(1).getHandRank());
+//		System.out.println("Player " + gameTable.getGamePlayers().get(2).getName() + " has " + gameTable.getGamePlayers().get(2).getHandRank());
+//		System.out.println("Player " + gameTable.getGamePlayers().get(3).getName() + " has " + gameTable.getGamePlayers().get(3).getHandRank());
+//		System.out.println("Winner " + gameTable.getWinner().getName() );
+//		
 		
 	
 	
@@ -100,7 +97,8 @@ public class tester {
 //		
 //		System.out.println(eval2.evaluate());
 //		
-		
+		GameEngine game = new GameEngine();
+		game.startGame();
 		
 		
 	}
